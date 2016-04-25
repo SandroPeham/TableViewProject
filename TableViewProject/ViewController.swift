74b740829346f20d9371f4abb9493f01461903af
Sandro Peham <peham.sandro@gmx.at>
2016-04-25 15:27:07 +0200

    Initial Commit

---
+++ TableViewProject/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TableViewProject
+//
+//  Created by Sandro Peham on 25.04.16.
+//  Copyright © 2016 Sandro Peham. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
